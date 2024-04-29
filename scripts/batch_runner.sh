5181973fe7d7aebc3a3368999b18b3305a6fa6aa
JohnVithor <jv.venceslau.c@gmail.com>
2024-04-29 20:27:51 -0300

    update gitignore and batch_runner

--- scripts/batch_runner.sh
+++ scripts/batch_runner.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+target=$1
 echo "version,seed,training_steps,reward_mean,reward_std"
 for seed in {0..99}
 do
-time python scripts/run_rust.py $seed
-time python scripts/run_python.py $seed
+time python scripts/run_rust_$target.py $seed
+time python scripts/run_python_$target.py $seed
 done
