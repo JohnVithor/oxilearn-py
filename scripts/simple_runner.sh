2f925b8d5e72d87f2f8fe4d6e2bede4e04f1a7d5
JohnVithor <jv.venceslau.c@gmail.com>
2024-05-07 15:56:43 -0300

    update simple_runner script

--- scripts/simple_runner.sh
+++ scripts/simple_runner.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 target=$1
-time python scripts/run_rust_$target.py
-time python scripts/run_python_$target.py
+seed=$2
+time python scripts/run_rust_$target.py $seed False 1
+time python scripts/run_python_$target.py $seed False 1
