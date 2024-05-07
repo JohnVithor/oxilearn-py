d28d808101575cfa821f678b69f8f35d43f36c10
JohnVithor <jv.venceslau.c@gmail.com>
2024-05-07 13:45:13 -0300

    update scripts / add jax algorithms

--- scripts/simple_task_runner.sh
+++ scripts/simple_task_runner.sh
@@ -2,4 +2,5 @@
 target=$1
 seed=$2
 time taskset --cpu-list 0 python scripts/run_rust_$target.py $seed false 1
-time taskset --cpu-list 0 python scripts/run_python_$target.py $seed false 1
+# time taskset --cpu-list 0 python scripts/run_python_$target.py $seed false 1
+# time taskset --cpu-list 0 python scripts/run_python_jax_$target.py $seed false 1
